6db80a92ef4948f27634b72be1084d70a5b4ad91
kunggenius123 <deryaan123@gmail.com>
2026-03-30 19:48:34 -0700

    Update text_analyzer

--- backend/text_analyzer.py
+++ backend/text_analyzer.py
@@ -85,6 +85,7 @@ def flagCheck(text):
             )
             triggered_text.append(sentence_text)
 
+
     #checking kincaid grade for each specific sentence itself
     for sentence in doc.sents:
         sentence_text = sentence.text.strip()
@@ -99,19 +100,6 @@ def flagCheck(text):
                 triggered_text.append(sentence_text)
         except Exception:
             flags.append("Readability score could not be calculated for a sentence.")
-
-    #checking for long words (avg syllables per word)
-    for sentence in doc.sents:
-        sentence_text = sentence.text.strip()
-        if not sentence_text:
-            continue
-        words = sentence_text.split()
-        if not words:
-            continue
-        long_words = [w for w in words if len(w) >= 10]
-        if len(long_words) >= 2:
-            flags.append(f"Long words detected {long_words[:3]}: {sentence_text}")
-            triggered_text.append(sentence_text)
 
     #checking for long words (avg syllables per word)
     for sentence in doc.sents:
